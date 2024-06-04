8a8db772a23720266ac4767992a32900f6b8eb9a
quincywalker1 <96150627+quincywalker1@users.noreply.github.com>
2024-06-04 10:32:55 -0500

    Add files via upload

--- footer.R
+++ footer.R
@@ -1,7 +1,7 @@
 footer <- function(){return(fluidRow(
   column(12, align = "center",
          tags$footer(
-           p("Copyright © 2024 My Shiny App"),
+           p("2024 Shiny App Portfolio"),
            p("Links:"),
            tags$a(href = "https://www.linkedin.com/in/quincy-walker-03a96a1a4?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app", "Linked In"),
            HTML("&nbsp;|&nbsp;"),
